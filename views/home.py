3065098c3932088d31138cd9bb1258bc7b038598
riana07 <31329303+riana07@users.noreply.github.com>
2026-01-14 07:39:45 +0000

    visitor map

--- views/home.py
+++ views/home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 def show_view():
     st.set_page_config(
@@ -17,5 +18,15 @@ def show_view():
         ⏱️ **Small jobs welcome — even 1 hour**  
         🛠️ **Trusted local professionals**
     """)
+    st.markdown("---")
+    st.subheader("🌍 Visitors Around the Globe")
 
+    st.markdown(
+    """
+    <a href="https://mapmyvisitors.com/web/1c1tk" title="Visit tracker" target="_blank">
+        <img src="https://mapmyvisitors.com/map.png?cl=5b5151&w=561&t=tt&d=cFREfn93-g8GoqabQ9uiD0Eo99ymx1ckkUA1n3O2SgQ&ct=dddddd&co=d8d8d8" alt="Visitor Map" style="width:100%; max-width:561px;">
+    </a>
+    """,
+    unsafe_allow_html=True
+)
  
